420eee25c06dbf49d8d16e7966e5170e52c926a8
MothyLag <decagufy30@gmail.com>
2020-04-01 08:52:25 -0600

    fixing fetch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import './App.sass';
-import Products from './components/Products';
+import React from "react";
+import "./App.sass";
+import Products from "./components/Products";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { root } from "./store/reducers/root.reducer";
+import Clientes from "./Pages/clientes/Clientes";
+
 function App() {
+  const store = createStore(root);
   return (
-    <div className="App">
-      <Products />
-    </div>
+    <Provider store={store}>
+      <Clientes /> ;
+    </Provider>
   );
 }
 
