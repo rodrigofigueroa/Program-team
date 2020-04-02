b4eb3b2ab20b3348b7fc39c2a340029cb180a599
MothyLag <decagufy30@gmail.com>
2020-04-02 12:01:13 -0600

    search

--- src/Pages/clients/Client.jsx
+++ src/Pages/clients/Client.jsx
@@ -3,6 +3,8 @@ import { Filter } from "../../components/filter/Filter";
 import { Table } from "../../components/table/Table";
 import { useSelector } from "react-redux";
 
+import Header from "../../components/header/Header";
+
 export default props => {
   const attrs = useSelector(state => state.ui.client);
   const [search, setSearch] = useState("");
@@ -11,48 +13,12 @@ export default props => {
     <div className="container-fluid">
       <div className="row">
         <div className="col-12">
-          <div className="card p-2 my-2 bg-info">
-            <div className="row">
-              <div className="col-3">
-                <h2 className="text-white">Clientes</h2>
-              </div>
-              <div className="col-3">
-                <select
-                  onChangeCapture={e => {
-                    console.log(e.target.value);
-                    setSearchAttr(e.target.value);
-                  }}
-                  id="searchSelect"
-                  name="searchSelect"
-                  autoComplete="off"
-                  type="select"
-                  className="form-control"
-                  aria-describedby="emailHelp"
-                >
-                  {Object.keys(attrs).map((option, index) => {
-                    return <option key={`option${index}`}>{option}</option>;
-                  })}
-                </select>
-              </div>
-              <div className="col-6">
-                <input
-                  onKeyPress={e => {
-                    if (e.key === "Enter") {
-                      setSearch(e.target.value);
-                    }
-                  }}
-                  id="searchInput"
-                  name="searchInput"
-                  defaultValue=""
-                  autoComplete="off"
-                  type="search"
-                  placeholder="buscar"
-                  className="form-control"
-                  aria-describedby="emailHelp"
-                />
-              </div>
-            </div>
-          </div>
+          <Header
+            catalogo="Clientes"
+            setSearchAttr={setSearchAttr}
+            attrs={attrs}
+            setSearch={setSearch}
+          />
         </div>
       </div>
 
