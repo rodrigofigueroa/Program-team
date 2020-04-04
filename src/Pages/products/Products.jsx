5ae58113eacd84161a3de036f3c19ab038e64a5f
MothyLag <decagufy30@gmail.com>
2020-04-04 17:01:21 -0600

    comentando

--- src/Pages/products/Products.jsx
+++ src/Pages/products/Products.jsx
@@ -1,45 +1,45 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Filter } from "../../components/filter/Filter";
 import { Table } from "../../components/table/Table";
 import { useSelector } from "react-redux";
 
 import Header from "../../components/header/Header";
-import {UPDATE_VISIBLE_PRODUCTS} from '../../store/actions/actions.vars';
-export default props => {  
-  const attrs = useSelector(state => state.ui.products);
+import { UPDATE_VISIBLE_PRODUCTS } from "../../store/actions/actions.vars";
+export default (props) => {
+  const attrs = useSelector((state) => state.ui.products);
   const [search, setSearch] = useState("");
   const [searchAttr, setSearchAttr] = useState("_id");
 
   const subCatalogues = [
     {
       component: {
-          api:"https://kapi-clavesadicionales.now.sh/",
-          catalogo:"clavesadicionales",
-          attrs:{ _id: true, nombre: true },
-          id:'clavesadicionales'
+        api: "https://kapi-clavesadicionales.now.sh/",
+        catalogo: "clavesadicionales",
+        attrs: { _id: true, nombre: true },
+        id: "clavesadicionales",
       },
       title: "Claves Adicionales",
-      selected: true
+      selected: true,
     },
     {
       component: {
-          api:"https://kapi-lineas.now.sh/",
-          catalogo:"lineas",
-          attrs:{ _id: true, nombre: true },
-          id:'lineas'
+        api: "https://kapi-lineas.now.sh/",
+        catalogo: "lineas",
+        attrs: { _id: true, nombre: true },
+        id: "lineas",
       },
       title: "Lineas",
-      selected: false
+      selected: false,
     },
     {
       component: {
-          api:"https://kapi-marcas.now.sh/",
-          catalogo:"marcas",
-          attrs:{ _id: true, nombre: true },
-          id:'marcas'
+        api: "https://kapi-marcas.now.sh/",
+        catalogo: "marcas",
+        attrs: { _id: true, nombre: true },
+        id: "marcas",
       },
       title: "Marcas",
-      selected: false
+      selected: false,
     },
   ];
 
@@ -58,7 +58,11 @@ export default props => {
 
       <div className="row">
         <div className="col-sm-12 col-md-2">
-          <Filter attrs={attrs} catalogue="Productos" type={UPDATE_VISIBLE_PRODUCTS} />
+          <Filter
+            attrs={attrs}
+            catalogue="Productos"
+            type={UPDATE_VISIBLE_PRODUCTS}
+          />
         </div>
         <div className="col-sm-12 col-md-10">
           <Table
